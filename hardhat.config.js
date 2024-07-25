aae1ee5c87ad58110d2d209e81779959e7b25cd9
Caio Melo <caio.videmelo@gmail.com>
2024-07-25 11:19:34 -0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
+require('dotenv').config({ path: './Teste.env' });
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24", // Versão principal
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    },
+    overrides: {
+      "contracts/Lock.sol": {
+        version: "0.8.24",
+        settings: {}
+      },
+      "contracts/NFT.sol": {
+        version: "0.8.24",
+        settings: {}
+      },
+      // Adicione outras versões conforme necessário
+      "@openzeppelin/contracts/*": {
+        version: "0.8.20",
+        settings: {}
+      }
+    }
+  },
+  networks: {
+    bsctestnet: {
+      url: process.env.BSC_TESTNET_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
